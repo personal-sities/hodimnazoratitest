b2c948104356c74643b49a17efa003ac8c919cb2
personal-sities <umirbekovdide@gmail.com>
2026-04-06 15:28:53 +0500

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,11 @@ const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 700,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      contextIsolation: false
     }
   });
 
